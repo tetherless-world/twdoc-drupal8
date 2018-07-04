f2b2739256ca9e6f7135947c62dd45d449f472a3
Patrick West <westp@rpi.edu>
2018-07-04 11:54:16 -0600

    handle move of js and css

--- packageup.sh
+++ packageup.sh
@@ -3,8 +3,12 @@
 /bin/rm -rf package
 
 mkdir -p package/web/modules/twdocs/common
+mkdir -p package/web/modules/twdocs/js
+mkdir -p package/web/modules/twdocs/css
 cp -R config src twdocs.info.yml twdocs.install twdocs.libraries.yml twdocs.links.menu.yml twdocs.module twdocs.routing.yml package/web/modules/twdocs
 cp common/TWDocs.inc package/web/modules/twdocs/common
+cp common/js/* package/web/modules/twdocs/js
+cp common/css/* package/web/modules/twdocs/css
 cp -R common/media package/media
 
 version=`git rev-parse --abbrev-ref HEAD`
